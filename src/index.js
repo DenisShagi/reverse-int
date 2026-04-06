38006de398876ef60fc9f9a20591811f4cb8817d
Denis <dd.shagimardanov@severstal.com>
2026-04-06 14:25:58 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const str = String(Math.abs(n));
+  const reversed = str.split('').reverse().join('');
+  const num = Number(reversed);
+
+  return num;
 };
